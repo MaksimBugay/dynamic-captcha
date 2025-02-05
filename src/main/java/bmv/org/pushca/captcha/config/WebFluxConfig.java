0bdd71929ae88a10db12741aad846631da38324c
maksim.bugai <maksim.bugai@multiloginapp.com>
2025-02-05 10:26:49 +0200

    custom ObjectMapper for serialization

--- src/main/java/bmv/org/pushca/captcha/config/WebFluxConfig.java
+++ src/main/java/bmv/org/pushca/captcha/config/WebFluxConfig.java
@@ -1,14 +1,22 @@
 package bmv.org.pushca.captcha.config;
 
+import bmv.org.pushca.captcha.serialization.json.JsonUtility;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.CodecConfigurer;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 import reactor.netty.http.HttpResources;
 import reactor.netty.resources.LoopResources;
 
 @Configuration
 @EnableWebFlux
-public class WebFluxConfig {
+public class WebFluxConfig implements WebFluxConfigurer {
 
   @Bean
   public HttpResources httpResources() {
@@ -20,5 +28,18 @@ public class WebFluxConfig {
             true
         )
     );
+  }
+
+  @Bean
+  public ObjectMapper customObjectMapper() {
+    return JsonUtility.getSimpleMapper();
+  }
+
+  @Override
+  public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+    ObjectMapper customObjectMapper = customObjectMapper();
+
+    configurer.defaultCodecs().jackson2JsonEncoder(new Jackson2JsonEncoder(customObjectMapper));
+    configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(customObjectMapper));
   }
 }
